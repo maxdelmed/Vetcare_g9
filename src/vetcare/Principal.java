94d6901a61b02857eae087ce6ebe97c606f67315
Max <fmaxde30742@ufide.ac.cr>
2026-08-19 13:47:43 -0600

    commit base

---
+++ src/vetcare/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vetcare;
+
+/**
+ *
+ * @author maxde
+ * Integrantes: Max Phillipe De La Fuente Medina, Karen Malespin Alvarado, Jeff Ian Miranda Garro
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+    }
+}
+
